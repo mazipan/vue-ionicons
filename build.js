1ad5289d4e7dbf41baf675ac27136a4f8f13c998
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-09-14 11:38:27 +0700

    init commit

---
+++ build.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const mustache = require('mustache')
+const path = require('path')
+
+const dist = path.resolve(__dirname, 'dist')
+const template = path.resolve(__dirname, 'template.mst')
+const svgPath = path.resolve(__dirname, 'ionicons/src')
+
+const svgs = fs.readdirSync(svgPath)
+
+const getPath = (svg) => {
+  const matches = /\sd="(.*)"/.exec(fs.readFileSync(path.join(svgPath, svg), {
+    encoding: 'utf8'
+  }))
+
+  if (matches) {
+    return matches[0]
+  }
+}
+
+const makeHumanReadable = (name) => {
+  let spacedName = name.split('-').join(' ')
+  humanReadableName = spacedName.charAt(0).toUpperCase() + spacedName.slice(1)
+  return humanReadableName
+}
+
+let templateData = svgs.map(svg => {
+  let name = svg.slice(0, -4)
+  return {
+    name: name,
+    readableName: makeHumanReadable(name),
+    path: getPath(svg)
+  } 
+})
+
+let componentFile = fs.readFileSync(template, { encoding: 'utf8'})
+
+if (!fs.existsSync(dist)) {
+  fs.mkdirSync(dist)
+}
+
+for (data of templateData) {
+  let component = mustache.render(componentFile, data)
+  let filename = data.name + ".vue"
+  fs.writeFileSync(path.resolve(dist, filename), component)
+}
