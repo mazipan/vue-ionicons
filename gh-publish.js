44a62f8b23cd5e81edfba50e2eae78d9e7544597
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-01-24 13:10:41 +0700

    [gh-pages] add color in console

--- gh-publish.js
+++ gh-publish.js
@@ -1,12 +1,13 @@
 const ghpages = require('gh-pages');
+const chalk = require('chalk')
 const BRANCH = 'gh-pages';
 const TODAY = new Date().toLocaleString();
+const VERSION = process.env.npm_package_version
 
-console.log(`start publishing to ${BRANCH}`);
-
+console.log(chalk.yellow(`start publishing v${VERSION} to ${BRANCH}`));
 ghpages.publish('demo/dist', {
   branch: BRANCH,
-  message: `Merge to ${BRANCH} ==> ${TODAY}`
+  message: `Merge v${VERSION} in ${TODAY}`
 }, () => {
-  console.log(`done publishing to ${BRANCH}`);
+  console.log(chalk.green(`done publishing v${VERSION} to ${BRANCH}`));
 });
