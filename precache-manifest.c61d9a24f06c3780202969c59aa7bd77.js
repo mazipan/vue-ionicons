8642d4f962fc0ce719099fb7060e6b19a85ecb27
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-01-04 18:01:14 +0700

    Merge v3.0.0 in 2019-1-4 18:00:55

---
+++ precache-manifest.c61d9a24f06c3780202969c59aa7bd77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd04ec11418bb58a2758918b1e4efe6e",
+    "url": "/vue-ionicons/index.html"
+  },
+  {
+    "revision": "47353f41a9ad4e4dcdaa",
+    "url": "/vue-ionicons/assets/js/runtime~index.47353f41.js"
+  },
+  {
+    "revision": "31d90baf87a49f3a0241",
+    "url": "/vue-ionicons/assets/js/index.31d90baf.chunk.js"
+  },
+  {
+    "revision": "4a8b15504e42c3f0053e",
+    "url": "/vue-ionicons/assets/js/chunk-vendors.4a8b1550.chunk.js"
+  },
+  {
+    "revision": "31d90baf87a49f3a0241",
+    "url": "/vue-ionicons/assets/css/index.31d90baf.chunk.css"
+  }
+];
--- sw.js
+++ sw.js
@@ -1,75 +1,27 @@
-importScripts('workbox-sw.prod.v2.1.2.js');
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/vue-ionicons/precache-manifest.c61d9a24f06c3780202969c59aa7bd77.js"
+);
 
 /**
- * DO NOT EDIT THE FILE MANIFEST ENTRY
- *
- * The method precache() does the following:
- * 1. Cache URLs in the manifest to a local cache.
- * 2. When a network request is made for any of these URLs the response
- *    will ALWAYS comes from the cache, NEVER the network.
- * 3. When the service worker changes ONLY assets with a revision change are
- *    updated, old cache entries are left as is.
- *
- * By changing the file manifest manually, your users may end up not receiving
- * new versions of files because the revision hasn't changed.
- *
- * Please use workbox-build or some other tool / approach to generate the file
- * manifest which accounts for changes to local files and update the revision
- * accordingly.
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
  */
-const fileManifest = [
-  {
-    "url": "/vue-ionicons/app.059863c6fbdb8a08453cc57399fb6cee.css",
-    "revision": "7ed611af3b868df2b69cd8b4ed623f44"
-  },
-  {
-    "url": "app.36f2704370abe9f588bc.js",
-    "revision": "1639b4dcb1f4eec47f9f6390935d8354"
-  },
-  {
-    "url": "app.62911c8c015f8636f5f480e86828b8ea.css",
-    "revision": "aa822280621268c42f6cde345d759030"
-  },
-  {
-    "url": "/vue-ionicons/app.79b85e0c12c085e37dd4.js",
-    "revision": "e4ac8d0d8c1887881cd70adbfd14561a"
-  },
-  {
-    "url": "app.8332d347c887f6c7dd25.js",
-    "revision": "e65795a2c389a946c0f643cb42d5579b"
-  },
-  {
-    "url": "app.a4200bc45732827d791e.js",
-    "revision": "c518205909a4ab133702ade47872af40"
-  },
-  {
-    "url": "/vue-ionicons/index.html",
-    "revision": "8fb51243e95b7e5b11aed824c2bbf6da"
-  },
-  {
-    "url": "/vue-ionicons/manifest.json",
-    "revision": "1124a7901152c484510ee86f1ddcfc5d"
-  },
-  {
-    "url": "vendor.36f2704370abe9f588bc.js",
-    "revision": "0baea52c112acda487b775190e1478aa"
-  },
-  {
-    "url": "/vue-ionicons/vendor.79b85e0c12c085e37dd4.js",
-    "revision": "6b341595a23a6fac1f7571317234f813"
-  },
-  {
-    "url": "vendor.8332d347c887f6c7dd25.js",
-    "revision": "a9d49902f372dd2d046f472df9cb0bb0"
-  },
-  {
-    "url": "vendor.a4200bc45732827d791e.js",
-    "revision": "922d884756bd8d347ae3e1453a89f63a"
-  }
-];
-
-const workboxSW = new self.WorkboxSW({
-  "skipWaiting": true,
-  "clientsClaim": true
-});
-workboxSW.precache(fileManifest);
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
