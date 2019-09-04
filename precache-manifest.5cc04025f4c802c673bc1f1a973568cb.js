2f5972b90fade461e74881cc701e049970965d3f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-04 16:01:46 +0000

    Deploy mazipan/vue-ionicons to github.com/mazipan/vue-ionicons.git:gh-pages

--- precache-manifest.5cc04025f4c802c673bc1f1a973568cb.js
+++ precache-manifest.5cc04025f4c802c673bc1f1a973568cb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = [
   {
-    "revision": "5d7138195d3c4946fd3286237a550cb7",
+    "revision": "8f05b792eb67abe518f09a207c91fbdb",
     "url": "/vue-ionicons/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/vue-ionicons/assets/js/runtime~index.47353f41.js"
   },
   {
-    "revision": "fe3c2f8d45c03cddc0f4",
-    "url": "/vue-ionicons/assets/js/index.fe3c2f8d.chunk.js"
+    "revision": "0439f19f8b3cf20027b5",
+    "url": "/vue-ionicons/assets/js/index.0439f19f.chunk.js"
   },
   {
     "revision": "4a8b15504e42c3f0053e",
     "url": "/vue-ionicons/assets/js/chunk-vendors.4a8b1550.chunk.js"
   },
   {
-    "revision": "fe3c2f8d45c03cddc0f4",
-    "url": "/vue-ionicons/assets/css/index.fe3c2f8d.chunk.css"
+    "revision": "0439f19f8b3cf20027b5",
+    "url": "/vue-ionicons/assets/css/index.0439f19f.chunk.css"
   }
 ];
