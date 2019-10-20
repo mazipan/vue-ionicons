f3ca9c95b2677bd2076b2b79d3e34bb10f2ccb07
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-20 02:40:42 +0000

    Deploy mazipan/vue-ionicons to github.com/mazipan/vue-ionicons.git:gh-pages

---
+++ precache-manifest.4ec751107a1e0818db697ae77165d5ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69762bd8a58fb18d54dfb2780effb915",
+    "url": "/vue-ionicons/index.html"
+  },
+  {
+    "revision": "47353f41a9ad4e4dcdaa",
+    "url": "/vue-ionicons/assets/js/runtime~index.47353f41.js"
+  },
+  {
+    "revision": "3c52c142447bb2fca1e5",
+    "url": "/vue-ionicons/assets/js/index.3c52c142.chunk.js"
+  },
+  {
+    "revision": "3770092d681e9f792f8f",
+    "url": "/vue-ionicons/assets/js/chunk-vendors.3770092d.chunk.js"
+  },
+  {
+    "revision": "3c52c142447bb2fca1e5",
+    "url": "/vue-ionicons/assets/css/index.3c52c142.chunk.css"
+  }
+];
